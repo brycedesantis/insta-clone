660a2e2d3ac3cc760802da36afd110d9a33a0402
Bryce DeSantis <potreader@hotmail.com>
2024-03-22 14:55:48 -0400

    Create login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,3 +1,35 @@
 export default function LoginPage() {
-	return <h1>Login</h1>
+	return (
+		<>
+			<div className="w-full h-screen bg-inherit">
+				<div className="flex justify-center items-center w-full h-full mt-8 pb-5">
+					{/* Promo image */}
+					<div>
+						<img src="/auth.png" alt="Promo Image" />
+					</div>
+
+					{/* Sign in form */}
+					<div className="flex flex-col items-center">
+						{/* Form placeholder */}
+						<h1>Login form</h1>
+						<section className="container flex flex-col items-center gap-4">
+							<h1>Get the app.</h1>
+							<div className="flex gap-2">
+								<img
+									className="h-11"
+									src="/appstore.png"
+									alt="App Store Link"
+								/>
+								<img
+									className="h-11"
+									src="/playstore.png"
+									alt="Play Store Link"
+								/>
+							</div>
+						</section>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
