f4896e40499b7a5a54813102a25b7eec79f2302d
Bryce DeSantis <potreader@hotmail.com>
2024-03-26 11:14:30 -0400

    Add base page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import LoginPage from "./pages/LoginPage"
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import BaseLayout from "./Layout/BaseLayout";
 
 function App() {
 	return (
-		<>
+		<BaseLayout>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/login" element={<LoginPage />} />
 			</Routes>
-		</>
-	)
+		</BaseLayout>
+	);
 }
 
-export default App
+export default App;
---
+++ src/Layout/BaseLayout.jsx
@@ -0,0 +1,22 @@
+import { useLocation } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+
+export default function BaseLayout({ children }) {
+	const { pathname } = useLocation();
+
+	return (
+		<div className="flex">
+			{/* Sidebar */}
+			{pathname !== "/login" ? (
+				<div className="w-[70px] md:w-60">
+					<Sidebar />
+				</div>
+			) : null}
+
+			{/* Content */}
+			<div className="flex-1 w-[calc(100%-70px)] md:w-[calc(100%-240px)]">
+				{children}
+			</div>
+		</div>
+	);
+}
