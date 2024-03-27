c3dc578437528928c99f350fc0eb7aee3677aa98
Bryce DeSantis <potreader@hotmail.com>
2024-03-27 10:24:53 -0400

    Structure home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,14 @@
 export default function HomePage() {
-	return <h1>Home</h1>
+	return (
+		<div className="container">
+			<div className="flex justify-center gap-14 w-full">
+				<div className="border border-blue-500 max-w-2xl w-full py-10">
+					Feed section
+				</div>
+				<div className="border border-red-500 hidden lg:block max-w-72 w-full mr-20">
+					suggestions section
+				</div>
+			</div>
+		</div>
+	);
 }
