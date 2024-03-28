67b322bfb1ac14f63659079c0e5c0d340fa97864
Bryce DeSantis <potreader@hotmail.com>
2024-03-28 14:21:12 -0400

    Create header for suggestion section

--- src/components/Suggested/SuggestedHeader.jsx
+++ src/components/Suggested/SuggestedHeader.jsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function SuggestedHeader() {
-	return <div>Suggested header</div>;
+	return (
+		<div className="flex justify-between items-center py-2">
+			<div className="flex items-center">
+				<img
+					className="size-9 rounded-full object-cover"
+					src="./profilepic.png"
+					alt="user profile picture"
+				/>
+
+				<p className="font-bold text-sm pl-2">brycedesant</p>
+			</div>
+			<div>
+				<Link to={"/login"} className="text-blue-500 text-sm hover:text-black">
+					Log out
+				</Link>
+			</div>
+		</div>
+	);
 }
